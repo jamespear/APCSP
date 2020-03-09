3074b7e67296f27599a0f2f302565484c866971b
jamespearce <jamespearce@172.17.84.60>
2020-03-09 11:55:37 -0700

    bruh moment

--- CaveStory/src/headers/Graphics.h
+++ CaveStory/src/headers/Graphics.h
@@ -1,16 +1,41 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+/*
+ * Graphics class
+ * Holds all information dealing with graphics
+ */
+
+#include <map>
+#include <string>
+
 struct SDL_Window;
 struct SDL_Renderer;
+struct SDL_Surface;
+struct SDL_Texture;
+struct SDL_Rect;
+
+
 
 class Graphics {
 public:
 	Graphics();
 	~Graphics();
+
+	SDL_Surface* loadImage(const std::string &filePath);
+
+	void blitSurface(SDL_Texture* source, SDL_Rect* sourceRectangle, SDL_Rect* destination);
+
+	void flip();
+
+	void clear();
+
+	SDL_Renderer* getRenderer() const;
 private:
 	SDL_Window *_window;
 	SDL_Renderer *_renderer;
+
+	std::map<std::string, SDL_Surface*> _spriteSheets;
 };
 
 #endif
--- CaveStory/src/source/Graphics.cpp
+++ CaveStory/src/source/Graphics.cpp
@@ -1,6 +1,6 @@
 #include <Graphics.h>
-
 #include "SDL2/SDL.h"
+#include <SDL2_image/SDL_image.h>
 
 Graphics::Graphics() {
 	SDL_CreateWindowAndRenderer(640, 480, 0, &this ->_window, &this ->_renderer);
@@ -10,4 +10,27 @@ Graphics::Graphics() {
 
 Graphics::~Graphics() {
 	SDL_DestroyWindow(this->_window);
+}
+
+SDL_Surface* Graphics::loadImage(const std::string &filePath) {
+	if(this->_spriteSheets.count(filePath) == 0) {
+		this -> _spriteSheets[filePath] = IMG_Load(filePath.c_str());
+	}
+	return this->_spriteSheets[filePath];
+	}
+
+void Graphics::blitSurface(SDL_Texture* texture, SDL_Rect* sourceRectangle, SDL_Rect* destinationRectangle) {
+	SDL_RenderCopy(this ->_renderer, texture, sourceRectangle, destinationRectangle);
+}
+
+void Graphics::flip() {
+	SDL_RenderPresent(this ->_renderer);
+}
+
+void Graphics::clear() {
+	SDL_RenderClear(this ->_renderer);
+}
+
+SDL_Renderer* Graphics::getRenderer() const {
+	return this->_renderer;
 }
