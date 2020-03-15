1e571cc54000baa312f679894dcfba1711455d91
jamespearce <jamespearce@jamess-mbp.fios-router.home>
2020-03-15 12:58:34 -0700

    another commit for some reason

---
+++ CaveStory/src/headers/animatedSprite.h
@@ -0,0 +1,50 @@
+#ifndef ANIMATED_SPRITE_H
+#define ANIMATED_SPRITE_H
+
+#include <globals.h>
+#include <sprite.h>
+#include <map>
+#include <vector>
+
+
+class Graphics;
+
+class AnimatedSprite: public Sprite {
+public:
+	AnimatedSprite();
+	AnimatedSprite(Graphics &graphics, const std::string &filePath, int sourceX, int sourceY, int width,
+			int height, float posX, float posY, float timeToUpdate);
+
+	void playAnimation(std::string animation, bool once = false);
+
+	void update(int elapsedTime);
+
+	void draw(Graphics &graphics, int x, int y);
+
+	virtual void setupAnimations();
+
+protected:
+	double _timeToUpdate;
+	bool _currentAnimationOnce;
+	std::string _currentAnimation;
+
+	void addAnimation(int frames, int x, int y, std::string name, int width, int height, Vector2 offset);
+
+	void resetAnimations();
+
+	void stopAnimation();
+
+	void setVisible(bool visible);
+
+	virtual void animationDone(std::string currentAnimation);
+
+private:
+	std::map<std::string, std::vector<SDL_Rect> > _animations;
+	std::map<std::string, Vector2> _offsets;
+
+	int _frameIndex;
+	double _timeElapsed;
+	bool _visible;
+};
+
+#endif
